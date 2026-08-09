964bfc5bcb829880cc87803c2a48a7feadd74be1
Patience <patience.andati@gmail.com>
2026-08-09 23:36:12 +0300

    Clean up folder-as-file issues and organize project files

--- rectangle.py/rectangle_calculator.py
+++
@@ -1,6 +0,0 @@
-x = float(input("Enter length: "))
-y = float(input("Enter width: "))
-
-print(x , y)
-print(x * y)
-print((x + y) * 2)
--- type_conversion.py/Desktop/learning-python/test.py
+++
@@ -1,6 +0,0 @@
-name = input("What's your name?")
-print("Hello, " + name)
-
-print("How old are you?")
-age = input()
-print("You are " + age + " years old")
