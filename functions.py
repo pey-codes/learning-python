881140eab179e287ee6a38cfcae68c54ccef8533
Patience <patience.andati@gmail.com>
2026-08-27 11:17:55 +0300

    functions.py

---
+++ functions.py
@@ -0,0 +1,19 @@
+def add(a, b):
+    return a + b
+answer = add(10, 65)
+print(answer)
+
+def subtract(a, b):
+    return a - b
+answer = subtract(10, 65)
+print(answer)
+
+def multiply(a, b):
+    return a * b
+answer = multiply(10, 65)
+print(answer)
+
+def divide(a, b):
+    return a / b
+answer = divide(10, 65)
+print(answer)
