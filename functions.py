2c6996a3f08c715eceb5ad4519ab165594f50d8b
Patience <patience.andati@gmail.com>
2026-08-27 12:29:12 +0300

    Add calculator project

--- functions.py
+++ functions.py
@@ -17,3 +17,20 @@ def divide(a, b):
     return a / b
 answer = divide(10, 65)
 print(answer)
+
+num1 = int(input("Enter first number:"))
+num2 = int(input("Enter second number:"))
+operation = input("Choose an operation(+, -, *, /):")
+
+if operation == "+":
+    answer = add(num1, num2)
+elif operation == "-":
+    answer = subtract(num1, num2)
+elif operation == "*":
+    answer = multiply(num1, num2)
+elif operation == "/":
+    answer = divide(num1, num2)
+else:
+    print("Invalid operation")
+
+print(answer)
