c07b54c7fca9c373d46c5838f86f073f3ffa5c5a
Patience <patience.andati@gmail.com>
2026-08-31 12:54:39 +0300

    Add lists practice

---
+++ lists.py
@@ -0,0 +1,17 @@
+friends = ["W", "X", "Y", "Z"]
+print(friends [0])
+
+friends = ["A", "B", "C", "D"]
+friends.append("E")
+print(friends)
+
+friends = ["E", "F", "G", "H","I"] 
+friends.remove("F")
+print(friends)
+
+clothes = ["dress", "skirt", "trouser", "jacket"]
+print(len(clothes))
+
+fruits = ["banana", "orange", "pineapple", "strawberry"]
+for item in fruits:
+    print(item)
