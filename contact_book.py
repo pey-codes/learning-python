7063cdc7e5f37d22f86ba064a982e6cb6374c08e
Patience <patience.andati@gmail.com>
2026-09-01 15:06:12 +0300

    Practice contact bood  with dictionaries

---
+++ contact_book.py
@@ -0,0 +1,12 @@
+contacts = []
+contact = {"name": "Peyshie",
+           "phone": "0723456789"}
+contacts.append(contact)
+print(contacts)
+
+name = input("Enter name: ")
+phone = input("Enter phone number: ")
+contact = {"name": name,
+           "phone": phone}
+contacts.append(contact)
+print(contacts)
