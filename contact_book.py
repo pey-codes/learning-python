2247c0200617e5928e2a4ccddb49480505668c3e
Patience <patience.andati@gmail.com>
2026-09-05 07:40:51 +0300

    Complete contact book

--- contact_book.py
+++ contact_book.py
@@ -4,9 +4,30 @@ contact = {"name": "Peyshie",
 contacts.append(contact)
 print(contacts)
 
-name = input("Enter name: ")
-phone = input("Enter phone number: ")
-contact = {"name": name,
+while True:
+    name = input("Enter name: ")
+    phone = input("Enter phone number: ")
+    contact = {"name": name,
            "phone": phone}
-contacts.append(contact)
-print(contacts)
+    contacts.append(contact)
+    again = input("Add another contact? (yes/no): ")
+    if again.lower() == "no":
+        break
+
+print("\n--- MY CONTACTS ---")
+
+for contact in contacts:
+        print("name", contact["name"])
+        print("phone", contact["phone"])
+
+search_name = input("\nEnter name to search: ")
+
+found = False
+for contact in contacts:
+     if contact["name"].lower() == search_name.lower():
+          print("contact found!")
+          print("name:", contact["name"])
+          print("phone:", contact["phone"])
+          found = True
+if found == False:
+     print("contact not found.")
